e06c1f0261bd1f9b6c15f1363a1ad9c13e93c768
lendfating <lizhen19900409@126.com>
2014-07-05 15:33:45 +0800

    31324

--- publish.bat
+++ publish.bat
@@ -4,6 +4,7 @@
 
 :: 用户名
 set usr=lendfating
+set BUILD_PATH=\workspace\lendfating.github.com
 
 :: 支持utf-8编码
 chcp 65001
@@ -12,23 +13,23 @@ chcp 65001
 set blog_type = %1
 if defined blog_type (
 	cd %blog_type%
-	call jekyll build
+	call jekyll build -d %BUILD_PATH%\%blog_type%
 ) else (
 	cd study
-	call jekyll build
+	call jekyll build -d %BUILD_PATH%\study
 	cd ../project
-	call jekyll build
+	call jekyll build -d %BUILD_PATH%\project
 	cd ../other
-	call jekyll build
+	call jekyll build -d %BUILD_PATH%\other
 )
 cd ..
 
 :: 如果有更新，则重新更新所有资源文件（图片等资源）
 :: /D:m-d-y 复制在指定日期或指定日期以后更改的文件,如果没有提供日期，只复制那些源时间比目标时间新的文件。
 :: /E 复制目录和子目录，包括空目录。
-copy home.html _site\index.html
-copy favicon.ico _site\favicon.ico
-xcopy assets _site\assets\ /D/E/Y
+copy home.html %BUILD_PATH%\index.html
+copy favicon.ico %BUILD_PATH%\favicon.ico
+xcopy assets %BUILD_PATH%\assets\ /D/E/Y
 
 :: 如果编译或拷贝出错，直接跳出
 if %errorlevel% NEQ 0  exit
